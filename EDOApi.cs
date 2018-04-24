8ab25a4c4199da2e6c55d4c274ea2a51696a956d
fazyloff <fazyloff@mail.ru>
2018-04-24 22:50:31 +0500

    24042018

--- EDOApi.cs
+++ EDOApi.cs
@@ -26,13 +26,26 @@ namespace EDOApi
     INVDOP,
     DOP
     }
+    public enum DocStatus
+    { 
+    New,
+    InProcess,
+    Completed,
+    Deleted
+    }
     public class Party 
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Inn { get; set; }
     }
     public class Document {
+        public string Id { get; set; } 
         public string Number { get; set; }
+        public Boolean IsDraft { get; set; }
         public DateTime Date { get; set; }
         public DocType Type { get; set; }
+        public DocStatus Status { get; set; }
         public Party Sender { get; set; }
         public Party Receiver { get; set; }
         public Decimal Amount { get; set; }
@@ -46,9 +59,9 @@ namespace EDOApi
         }
          string Login { get; set; }
          string Password { get; set; }
-         Boolean Connect();
-       
-        
+         Boolean  Connect();
+         Document getDocument(string Id);
+         List<Document> getDocumentList(DateTime startDate, DateTime endDate);
     }
     public class EDOConnector : IEDOConnector
     {
@@ -60,7 +73,7 @@ namespace EDOApi
         public string Login { get; set; }
         public string Password { get; set; }
         private string Secret { get; set; }
-        public Boolean Connect()
+        public virtual Boolean Connect()
         {
             return false;
         }
@@ -85,13 +98,21 @@ namespace EDOApi
                     }
             }
         }
+        public virtual Document getDocument(string Id)
+        {
+        return new Document();
+        }
+        public virtual List<Document> getDocumentList()
+        { 
+        return new List<Document>();
+        }
     }
     public class CourierConnector : EDOConnector, IEDOConnector
     {
         public const EdoOperator Operator = EdoOperator.Courier;
         private Korus.Eds.Api.RestClient  client;
         private string token;
-        public Boolean Connect()
+        public override Boolean Connect()
         {
             RestSharp.IRestResponse<Korus.Eds.Api.LogonResponse> resp = client.Login(this.Login, this.Password);
             if (resp.StatusCode == System.Net .HttpStatusCode.OK)
@@ -113,15 +134,16 @@ namespace EDOApi
     public class DiadocConnector :EDOConnector , IEDOConnector
     {
         public const EdoOperator Operator =EdoOperator.Diadoc;
-        public const string apiUrl = "a";
+        public const string apiUrl = "https://diadoc-api.kontur.ru";
         private Diadoc.Api.Cryptography.WinApiCrypt crypt;
         public string ApiKey { get; set; }
         private string token;
         private Diadoc.Api.DiadocApi api;
-        public Boolean Connect()
+        public override Boolean Connect()
         {
             try
             {
+                
                 this.token = api.Authenticate(this.Login, this.Password);
             }
             catch
@@ -135,7 +157,9 @@ namespace EDOApi
         {
             try
             {
-                api = new Diadoc.Api.DiadocApi(ApiKey, apiUrl, crypt);
+                this.ApiKey = "gradient-c5a5fb07-eafa-495c-9240-557096c92fff";
+                this.crypt = new Diadoc.Api.Cryptography.WinApiCrypt();
+                this.api = new Diadoc.Api.DiadocApi(this.ApiKey, apiUrl, this.crypt);
             }
             catch
             { 
